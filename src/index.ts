dd69896325a29d9d65452d5ca61142979a7e6536
riz1-lv <riz.shkh1@gmail.com>
2021-04-15 22:38:18 -0400

    started setting up MikroORM and Entities

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello yes yes yes")
+import { MikroORM } from "@mikro-orm/core"
+import { __prod__ } from "./constants";
+
+const main = async() =>{
+
+const orm = await MikroORM.init({ entities: ['./entities'], // path to your JS entities (dist), relative to `baseDir`
+dbName: 'forumme',
+type: 'postgresql',
+debug: !__prod__,
+clientUrl: '...',})
+
+
+}
+
+main();
