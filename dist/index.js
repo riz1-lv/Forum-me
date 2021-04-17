ac5df70997d7f12fe8326f78e5aa7c0f49f656be
riz1-lv <riz.shkh1@gmail.com>
2021-04-17 17:12:15 -0400

    setup db and MikroOrm

--- dist/index.js
+++ dist/index.js
@@ -14,15 +14,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require('dotenv').config();
 const core_1 = require("@mikro-orm/core");
-const Post_1 = require("./entities/Post");
 require("../.env");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
-    console.log(process.env.DATABASE_PASSWORD);
     const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
     yield orm.getMigrator().up();
-    const post = orm.em.create(Post_1.Post, { title: ' db Test Post' });
-    yield orm.em.persistAndFlush(post);
 });
 main();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 require('dotenv').config()
+
 import { MikroORM } from "@mikro-orm/core"
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
@@ -6,11 +7,11 @@ import '../.env'
 import MicroConfig from './mikro-orm.config'
 
 const main = async() =>{
-console.log(process.env.DATABASE_PASSWORD)
 const orm = await MikroORM.init(MicroConfig)
-
 await orm.getMigrator().up();
-const post = orm.em.create(Post,{title:' db Test Post'})
-await orm.em.persistAndFlush(post);
+// const post = orm.em.create(Post,{title:' db Test Post'})
+// await orm.em.persistAndFlush(post);
+// const post = await orm.em.find(Post,{})
+// console.log(post)
 }
-main()
+main();
