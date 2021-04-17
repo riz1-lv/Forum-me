bf71fe5e9f9b4af14d18917b95d21f30fbf2ec81
riz1-lv <riz.shkh1@gmail.com>
2021-04-16 21:09:54 -0400

    added database schema

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
+require('dotenv').config()
 import { MikroORM } from "@mikro-orm/core"
 import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import '../.env'
+import MicroConfig from './mikro-orm.config'
 
 const main = async() =>{
+console.log(process.env.DATABASE_PASSWORD)
+const orm = await MikroORM.init(MicroConfig)
 
-const orm = await MikroORM.init({ entities: ['./entities'], // path to your JS entities (dist), relative to `baseDir`
-dbName: 'forumme',
-type: 'postgresql',
-debug: !__prod__,
-clientUrl: '...',})
-
-
+await orm.getMigrator().up();
+const post = orm.em.create(Post,{title:' db Test Post'})
+await orm.em.persistAndFlush(post);
 }
-
-main();
+main()
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,17 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import {MikroORM} from "@mikro-orm/core"
+import path from 'path'
+
+export default{ 
+  migrations: {
+    path: path.join(__dirname,'./migrations'),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
+  entities: [Post], // path to your JS entities (dist), relative to `baseDir`
+  dbName: 'forumme',
+  type: 'postgresql',
+  debug: !__prod__,
+  password: process.env.DATABASE_PASSWORD,
+  } as Parameters<typeof MikroORM.init>[0];
+   
